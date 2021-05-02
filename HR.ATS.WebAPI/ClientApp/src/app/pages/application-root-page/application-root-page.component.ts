cf0723d77ec8b4ebe0d7055ac6f1a972fb8a8e8f
Pedro Ballona <pedro.ballona@totvs.com.br>
2021-05-02 09:38:58 -0300

    Adicionado botão de logout

--- HR.ATS.WebAPI/ClientApp/src/app/pages/application-root-page/application-root-page.component.ts
+++ HR.ATS.WebAPI/ClientApp/src/app/pages/application-root-page/application-root-page.component.ts
@@ -1,31 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-import { OAuthService } from 'angular-oauth2-oidc';
+import { Component } from '@angular/core';
+import { PoToolbarAction, PoToolbarProfile } from '@po-ui/ng-components';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { AuthService } from '../../utils/auth.service';
 import { MenuService } from '../../utils/menu.service';
 
 @Component({
   selector: 'app-application-root-page',
   templateUrl: './application-root-page.component.html',
-  styleUrls: ['./application-root-page.component.css']
+  styleUrls: ['./application-root-page.component.css'],
 })
-export class ApplicationRootPageComponent implements OnInit {
-
-  title = 'ats';
-  token = '';
-
-  constructor(private oauthService: OAuthService, private menuService: MenuService) {}
+export class ApplicationRootPageComponent {
+  constructor(
+    private authService: AuthService,
+    private menuService: MenuService
+  ) {}
 
   readonly menus$ = this.menuService.menu$;
-
-  private onClick(): void {
-    alert('Clicked in menu item');
-  }
-
-  async ngOnInit(): Promise<void> {
-    this.token = this.oauthService.authorizationHeader();
-  }
-
-  logout(): void {
-    this.oauthService.logOut();
-  }
-
+  readonly profile$: Observable<
+    PoToolbarProfile | undefined
+  > = this.authService.state$.pipe(
+    map((user) => {
+      if (!user) {
+        return undefined;
+      }
+      return { title: user.name };
+    })
+  );
+  readonly profileActions: PoToolbarAction[] = [
+    {
+      label: 'Sair',
+      action: () => {
+        this.authService.logout();
+      }
+    }
+  ];
 }
